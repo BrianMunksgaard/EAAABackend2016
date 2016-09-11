3370e7ad0f5337a3a635b44d53369d3dd6192b27
BrianMunksgaard <F15Eagle!>
2016-09-11 16:01:23 +0200

    .

---
+++ Lessons/Lesson 2/DiceRoll/DiceRoll/DiceRoll/Controllers/DiceController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DiceRoll.Models;
+
+namespace DiceRoll.Controllers
+{
+    public class DiceController : Controller
+    {
+         // GET: Dice
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+            ViewBag.Dice = RollDice();
+            return View();
+        }
+
+        private Dice RollDice()
+        {
+            Dice dice;
+            if (Session["dice"] == null)
+            {
+                dice = new Dice();
+                dice.Roll();
+                Session["dice"] = dice;
+            }
+            else
+            {
+                dice = (Dice)Session["dice"];
+                dice.Roll();
+                Session["dice"] = dice;
+            }
+            return dice;
+        }
+    }
+}
---
+++ Lessons/Lesson 2/DiceRoll/DiceRoll/DiceRoll/Models/Dice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceRoll.Models
+{
+    public class Dice
+    {
+        private Random r = new Random();
+
+        private int eyes;
+        private int numRolls;
+        private int outcome;
+        private int totalSum;
+        private int[] distribution;
+
+        public Dice()
+        {
+            eyes = 6;
+            numRolls = 0;
+            totalSum = 0;
+            distribution = new int[eyes];
+        }
+
+        public void Roll()
+        {
+            //The numRolls is the total number of rolls of the dice.
+            //The outcome is what the dice landed at, and totalSum is
+            //the total sum of eyes in all dice rolls.
+            //The distribution field is used to keep track of how many
+            //times the dice landed on each of the possible outcomes (1, 2, 3, 4, 5, and 6):
+            numRolls++;
+            outcome = r.Next(1, 6);
+            totalSum += outcome;
+
+            int index = outcome - 1;
+            distribution[index]++;
+        }
+    }
+}
