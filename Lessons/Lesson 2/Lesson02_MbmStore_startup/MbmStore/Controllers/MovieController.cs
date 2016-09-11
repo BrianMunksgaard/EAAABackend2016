558e52f2262feb1b73b51f48252edcd1c0eb09fc
BrianMunksgaard <F15Eagle!>
2016-09-11 13:30:10 +0200

    Two more movies added.

--- Lessons/Lesson 2/Lesson02_MbmStore_startup/MbmStore/Controllers/MovieController.cs
+++ Lessons/Lesson 2/Lesson02_MbmStore_startup/MbmStore/Controllers/MovieController.cs
@@ -15,10 +15,13 @@ namespace MbmStore.Controllers
             
             // create a new Movie object with instance name jungleBook
             Movie jungleBook = new Movie("Jungle Book", 160.50m, "junglebook.jpg");
+            Movie forrestGump = new Movie("Forrest Gump", 125.00m, "forrest-gump.jpg");
+            Movie gladiator = new Movie("Gladiator", 149.75m, "gladiator.jpg");
 
             // assign a viewbag property to the new Movie object
             ViewBag.JungleBook = jungleBook;
-
+            ViewBag.ForrestGump = forrestGump;
+            ViewBag.Gladiator = gladiator;
 
             return View();
         }
