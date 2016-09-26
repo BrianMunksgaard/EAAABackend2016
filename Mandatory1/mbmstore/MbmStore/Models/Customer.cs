6c648c5d33f7d71e939228cb4d61e3c2dcec0e84
BrianMunksgaard <F15Eagle!>
2016-09-27 00:09:02 +0200

    OrderItem and Invoice classes added. Refactored customer class.

--- Mandatory1/mbmstore/MbmStore/Models/Customer.cs
+++ Mandatory1/mbmstore/MbmStore/Models/Customer.cs
@@ -7,13 +7,70 @@ namespace MbmStore.Models
 {
     public class Customer
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string Zip { get; set; }
-        public string City { get; set; }
 
+        #region PrivateFields
+
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string zip;
+        private string city;
         private DateTime birthDay;
+        private List<string> phoneNumbers;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Customer first name.
+        /// </summary>
+        public string FirstName
+        {
+            get { return firstName == null ? firstName = string.Empty : firstName; }
+            set { firstName = value; }
+        }
+
+        /// <summary>
+        /// Customer last name.
+        /// </summary>
+        public string LastName
+        {
+            get { return lastName == null ? lastName = string.Empty : lastName; }
+            set { lastName = value; }
+        }
+
+        /// <summary>
+        /// Customer address.
+        /// </summary>
+        public string Address
+        {
+            get { return address == null ? address = string.Empty : address; }
+            set { address = value; }
+        }
+
+        /// <summary>
+        /// Customer zip code.
+        /// </summary>
+        public string Zip
+        {
+            get { return zip == null ? zip = string.Empty : zip; }
+            set { zip = value; }
+        }
+
+        /// <summary>
+        /// Customer city.
+        /// </summary>
+        public string City
+        {
+            get { return city == null ? city = string.Empty : city; }
+            set { city = value; }
+        }
+
+        /// <summary>
+        /// Customer birthday. Birthday has to be
+        /// between 0 and 120 years, both included.
+        /// </summary>
         public DateTime BirthDay
         {
             get { return birthDay; }
@@ -31,6 +88,9 @@ namespace MbmStore.Models
             }
         }
 
+        /// <summary>
+        /// Returns the customer age in a string.
+        /// </summary>
         public string Age
         {
             get
@@ -54,8 +114,10 @@ namespace MbmStore.Models
                 }
             }
         }
-        
-        private List<string> phoneNumbers;
+
+        /// <summary>
+        /// The list of customer phone numbers.
+        /// </summary>
         public List<string> PhoneNumbers
         {
             get
@@ -68,6 +130,10 @@ namespace MbmStore.Models
             }
         }
 
+        /// <summary>
+        /// Return all phone numbers in a concatinated string.
+        /// Numbers are seperated by comma.
+        /// </summary>
         public string AllPhoneNumbers
         {
             get
@@ -83,6 +149,16 @@ namespace MbmStore.Models
             }
         }
 
+        #endregion
+
+        /// <summary>
+        /// Initialization constructor.
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="zip"></param>
+        /// <param name="city"></param>
         public Customer(string firstName, string lastName, string address, string zip, string city)
         {
             FirstName = firstName;
@@ -92,6 +168,10 @@ namespace MbmStore.Models
             City = city;
         }
 
+        /// <summary>
+        /// Add customer phone number.
+        /// </summary>
+        /// <param name="phoneNumber"></param>
         public void AddPhoneNumber(string phoneNumber)
         {
             if(!string.IsNullOrEmpty(phoneNumber)) {
