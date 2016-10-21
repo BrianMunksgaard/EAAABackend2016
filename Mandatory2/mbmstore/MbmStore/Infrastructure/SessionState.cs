60dd8ec8d2bced81502aa5f2ad77025ebc3a8b76
BrianMunksgaard <brian.munksgaard@gmail.com>
2016-10-21 14:20:23 +0200

    Refactoring/Code cleanup.

--- Mandatory2/mbmstore/MbmStore/Infrastructure/SessionState.cs
+++ Mandatory2/mbmstore/MbmStore/Infrastructure/SessionState.cs
@@ -11,34 +11,11 @@ namespace MbmStore.Infrastructure
     {
         #region PrivateFields
 
-        //private Repository repository;
         private Customer currentCustomer;
-        //private Cart shoppingCart;
 
         #endregion
 
         #region Properties
-
-        /// <summary>
-        /// Reference to the reporistory.
-        /// </summary>
-        /*
-        public Repository Repository
-        {
-            get
-            {
-                if(repository == null)
-                {
-                    repository = new Repository();
-                }
-                return repository;
-            }
-            set
-            {
-                repository = value;
-            }
-        }
-        */
 
         /// <summary>
         /// The current customer.
@@ -49,15 +26,6 @@ namespace MbmStore.Infrastructure
             set { currentCustomer = value; }
         }
 
-        /// <summary>
-        /// The current shopping cart.
-        /// </summary>
-        /*
-        public Cart ShoppingCart
-        {
-            get { return shoppingCart == null ? shoppingCart = new Cart() : shoppingCart; }
-        }
-        */
         #endregion
 
         /// <summary>
