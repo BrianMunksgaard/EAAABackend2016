c73ded981dcf65a6d3e819b6ea50518985110d7f
BrianMunksgaard <brian.munksgaard@gmail.com>
2016-10-27 23:58:46 +0200

    .

--- Mandatory2/mbmstore/MbmStore/Controllers/CatalogueController.cs
+++ Mandatory2/mbmstore/MbmStore/Controllers/CatalogueController.cs
@@ -9,6 +9,10 @@ using System.Web.Mvc;
 
 namespace MbmStore.Controllers
 {
+    /// <summary>
+    /// The catalogue controller is used when browsing
+    /// products in the store.
+    /// </summary>
     public class CatalogueController : Controller
     {
         /// <summary>
@@ -22,24 +26,11 @@ namespace MbmStore.Controllers
         private Repository repository = Repository.Instance;
 
         /// <summary>
-        /// Returns a view with all the product data
-        /// from the repository.
+        /// Retrieves product data from the repository
+        /// and returns data to the view. Category and
+        /// page is used to subset the data retrieved.
         /// </summary>
         /// <returns></returns>
-        /*
-        public ActionResult Index()
-        {
-            // Get reference to products repository. The repository
-            // holds all data for the application.
-            List<Product> products = repository.Products;
-
-            // Store data in the view model.
-            CatalogueViewModel vm = new CatalogueViewModel(products);
-
-            // Return the model to the view.
-            return View(vm);
-        }*/
-
         public ActionResult Index(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
