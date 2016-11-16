e57bc462930c91f1e8931e93e66dd4918b58abeb
bm <bm@PCBM-X230>
2016-11-16 10:58:54 +0100

    Exercise 1.

---
+++ Lessons/Lesson 10/Lesson10Exercises/Lesson10Exercises/Controllers/CreateBookingController.cs
@@ -0,0 +1,30 @@
+﻿using Lesson10Exercises.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson10Exercises.Controllers
+{
+    public class CreateBookingController : Controller
+    {
+        // GET: CreateBooking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create([Bind(Include = "Name,Username,Password,ConfirmPassword,Age")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("RegistrationCompleted", user);
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+    }
+}
---
+++ Lessons/Lesson 10/Lesson10Exercises/Lesson10Exercises/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lesson10Exercises.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(1, 120)]
+        public int Age { get; set; }
+    }
+}
