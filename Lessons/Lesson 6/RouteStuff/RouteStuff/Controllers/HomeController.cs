99aca948e8da9d90dddfa0fed6fbe608d7010935
bm <bm@PCBM-X230>
2017-02-17 12:18:52 +0100

    .

---
+++ Lessons/Lesson 6/RouteStuff/RouteStuff/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RouteStuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index(int id = 0)
+        {
+            return id.ToString();
+            //return View();
+        }
+    }
+}
